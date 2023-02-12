6492e812abae187d7616e7c5cb7cd92484a7e6f2
Weronika <laskowska29@gmail.com>
2023-02-12 16:36:23 +0100

    initial

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+
+
+<template>
+    <div class="container" id="play-again">
+        <h2 id="turn" class="neon" data-text="x's turn!">x's turn!</h2>
+        <a href="#" class="btn neon-button">PLAY AGAIN</a>
+    </div>
+    <h1 id="turn" class="neon">x's turn!</h1>
+<div class="board">
+    <div class="field" id="top-left"></div>
+    <div class="field" id="top-middle"></div>
+    <div class="field" id="top-right"></div>
+    <div class="field" id="middle-left"></div>
+    <div class="field" id="middle-middle"></div>
+    <div class="field" id="middle-right"></div>
+    <div class="field" id="bottom-left"></div>
+    <div class="field" id="bottom-middle"></div>
+    <div class="field" id="bottom-right"></div>
+</div>
+</template>
+<script >
+
+</script>
+
+
+
+<style scoped>
+
+
+</style>
